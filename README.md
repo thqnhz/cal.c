22ccd9cb57f7ca04b5d2be1218eecd66010ab3b7
ThanhZ <89237062+thqnhz@users.noreply.github.com>
2026-03-28 16:44:47 +0700

    feat: basic app drawing

--- README.md
+++ README.md
@@ -2,3 +2,24 @@
 
 Calculator in C
 
+## Build from source
+
+1. Clone the project
+
+```bash
+$ git clone https://github.com/thqnhz/cal.c.git
+$ cd cal.c
+```
+
+2. Bootstrap nob.h (Only need to do this once)
+
+```bash
+$ gcc -o nob nob.c
+```
+
+3. Run the app
+
+```bash
+$ ./build/Calc
+```
+
--- src/cal.c
+++ src/cal.c
@@ -1,7 +1,54 @@
-#include <stdio.h>
+#include "../include/raylib.h"
+
+#define WINW 360
+#define WINH 720
+#define APP_NAME "Calc"
+#define WIN_PADDING 8.0f
+#define PADDING (WIN_PADDING * 2.0f)
+#define CALC_HEADER 40.0f
+#define CALC_DISPLAY_H 72.0f
+
+static const Color CALC_THEME_BASE = (Color){ 30, 30, 46, 255 };
+static const Color CALC_THEME_MANTLE = (Color){ 24, 24, 37, 255 };
+static const Color CALC_THEME_CRUST = (Color){ 17, 17, 27, 255 };
+static const Color CALC_THEME_TEXT = (Color){ 205, 214, 244, 255 };
+
+static const Rectangle CALC_BG = (Rectangle){ .x      = WIN_PADDING,
+                                              .y      = WIN_PADDING,
+                                              .width  = WINW - WIN_PADDING * 2.0f,
+                                              .height = WINH - WIN_PADDING * 2.0f };
+static const Rectangle CALC_DISPLAY = (Rectangle){ .x      = PADDING,
+                                                   .y      = PADDING + CALC_HEADER,
+                                                   .width  = WINW - PADDING * 2.0f,
+                                                   .height = CALC_DISPLAY_H };
+
+static inline void draw_background();
+static inline void draw_display();
+
+static inline void draw_app() {
+    ClearBackground(CALC_THEME_BASE);
+    draw_background();
+    draw_display();
+}
+
+static inline void draw_background() {
+    DrawRectangleRounded(CALC_BG, .05f, 0, CALC_THEME_MANTLE);
+}
+
+static inline void draw_display() {
+    DrawRectangleRounded(CALC_DISPLAY, 0.05f, 0, CALC_THEME_CRUST);
+}
 
 int main() {
-    printf("Hello, world!\n");
+    InitWindow(WINW, WINH, APP_NAME);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        draw_app();
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
 
