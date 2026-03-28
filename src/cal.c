a0d9a8986a80445030c456f02a20a99c60674ced
ThanhZ <89237062+thqnhz@users.noreply.github.com>
2026-03-28 17:44:56 +0700

    feat: font, resource dir finder, some display text drawing

--- src/cal.c
+++ src/cal.c
@@ -1,8 +1,10 @@
+#include "../include/resource_dir.h"
 #include "../include/raylib.h"
 
 #define WINW 360
 #define WINH 720
 #define APP_NAME "Calc"
+#define APP_NAME_FULL "Calculator"
 #define WIN_PADDING 8.0f
 #define PADDING (WIN_PADDING * 2.0f)
 #define CALC_HEADER 40.0f
@@ -23,11 +25,14 @@ static const Rectangle CALC_DISPLAY = (Rectangle){ .x      = PADDING,
                                                    .height = CALC_DISPLAY_H };
 
 static inline void draw_background();
+static inline void draw_header(Font font);
 static inline void draw_display();
+static inline void draw_display_text_result(Font font, const char *text, float font_size);
 
-static inline void draw_app() {
+static inline void draw_app(Font font) {
     ClearBackground(CALC_THEME_BASE);
     draw_background();
+    draw_header(font);
     draw_display();
 }
 
@@ -35,16 +40,30 @@ static inline void draw_background() {
     DrawRectangleRounded(CALC_BG, .05f, 0, CALC_THEME_MANTLE);
 }
 
+static inline void draw_header(Font font) {
+    Vector2 text_size = MeasureTextEx(font, APP_NAME_FULL, 32.0f, .0f);
+    Vector2 draw_pos = { (WINW - text_size.x) / 2.0f, PADDING + (CALC_HEADER - text_size.y) / 2.0f };
+    DrawTextEx(font, APP_NAME_FULL, draw_pos, 32.0f, .0f, CALC_THEME_TEXT); }
+
 static inline void draw_display() {
     DrawRectangleRounded(CALC_DISPLAY, 0.05f, 0, CALC_THEME_CRUST);
+}
+
+static inline void draw_display_text_result(Font font, const char *text, float font_size) {
+    Vector2 text_size = MeasureTextEx(font, text, font_size, 0.0f);
 }
 
 int main() {
     InitWindow(WINW, WINH, APP_NAME);
 
+    if (!SearchAndSetResourceDir("assets"))
+        TraceLog(LOG_WARNING, "Failed to set resource directory, fonts won't load correctly");
+    Font font = LoadFontEx(TextFormat("%s/%s", GetWorkingDirectory(), "Rubik/Rubik-Regular.ttf"), 72, 0, 0);
+
     while (!WindowShouldClose()) {
         BeginDrawing();
-        draw_app();
+        draw_app(font);
+        DrawTextEx(font, "0", (Vector2){ PADDING + 4.0f, PADDING + CALC_HEADER }, 36.0f, .0f, CALC_THEME_TEXT);
         EndDrawing();
     }
 
